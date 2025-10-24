b7848f915566f5a09868086831adc742ce4dea2c
Peter Toussaint <dj.peterpiper1@gmail.com>
2025-10-24 14:25:31 -0400

    Initialize React frontend

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,14 +1,18 @@
+// webpack.config.js
 const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   entry: "./src/index.js",
-
   output: {
     path: path.join(__dirname, "/dist"),
     filename: "bundle.js",
   },
-  plugins: [new HTMLWebpackPlugin({ template: "./public/index.html" })],
+  plugins: [
+    new Dotenv(),
+    new HTMLWebpackPlugin({ template: "./public/index.html" }),
+  ],
   module: {
     rules: [
       {
@@ -21,22 +25,25 @@ module.exports = {
           },
         },
       },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.png$/,
-        use: "file-loader",
-      },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      { test: /\.png$/, use: "file-loader" },
     ],
   },
-resolve: {
-  extensions: ['.js', '.jsx'], // Automatically resolve these extensions
-},
-
-  devServer: {
-    port: 3000, // Specify the desired port here
+  resolve: {
+    extensions: [".js", ".jsx"],
   },
-
+  devServer: {
+    port: 3000,
+    hot: true,
+    open: true,
+    proxy: [
+      {
+        context: ["/api"],          // which requests to proxy
+        target: "http://localhost:4000",
+        secure: false,              // if your backend is HTTPS with self-signed certs
+        changeOrigin: true,         // rewrite the Host header to the target URL
+        // pathRewrite: { "^/api": "" }  // if your backend routes don’t include the /api prefix
+      }
+    ],
+  },
 };
